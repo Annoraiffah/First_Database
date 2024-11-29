2dbdae4cd87ef3e0d0259e0df7cfcfe34a0a56c8
Annoraiffah <163631207+Annoraiffah@users.noreply.github.com>
2024-11-29 15:39:08 +0700

    Add files via upload

---
+++ buku_tamu.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Nov 2024 pada 08.59
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tugas_pemweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku_tamu`
+--
+
+CREATE TABLE `buku_tamu` (
+  `ID_BT` int(10) NOT NULL,
+  `NAMA` varchar(200) NOT NULL,
+  `EMAIL` varchar(50) NOT NULL,
+  `ISI` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `buku_tamu`
+--
+
+INSERT INTO `buku_tamu` (`ID_BT`, `NAMA`, `EMAIL`, `ISI`) VALUES
+(1, 'Annora Iffah Diniyah Darma', 'annoraiffah@gmail.com', 'Tamu pertama'),
+(2, 'Ardelia Belvana', 'ardeliabelva@gmail.com', 'Tamu kedua');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  ADD PRIMARY KEY (`ID_BT`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  MODIFY `ID_BT` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
